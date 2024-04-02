088a220d7c23537d9603e3d0961bab995f1a0631
LipeAbreu <126911861+LipeAbreu@users.noreply.github.com>
2024-04-02 16:02:33 -0300

    Adicionando o app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let nome = prompt('digite o seu nome');
+let numero1 = prompt ('qual o primeiro numero')
+let numero2 = prompt ('qual o segundo numero')
+
+console.log(`Olá, ${nome}`);
+operaçãoSecreta();
+
+function operaçãoSecreta(numero1,numero2) {
+    numero1 = a;
+    numero2 = b;
+    let resultadoSecreto = (a + b);
+    console.log(`o resultado de ${a} vezes ${b}, é igual a ${resultadoSecreto}`);
+}
