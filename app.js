74f0cb8ffa91e2a8c0cbb8849c387f4d73da2a48
LipeAbreu <126911861+LipeAbreu@users.noreply.github.com>
2024-04-05 20:26:55 -0300

    Resolvido Graças a Jesus

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ let numero1 = parseInt(prompt('qual o primeiro número'));
 let numero2 = parseInt(prompt('qual o segundo número'));
 let numero3 = parseInt(prompt('qual o terceiro número'));
 let numero4 = parseInt(prompt('qual o quarto número'));
+let protagonista = parseFloat(prompt('Digite o nome de um protagonista'));
+let rival = parseFloat(prompt('Digite o nome de um rival'));
+let pet = parseFloat(prompt('Digite o nome de um pet'));
+let figurante = parseFloat(prompt('Digite o nome de um figurante'));
+let poder1 = parseFloat(prompt('Digite o nome de um poder ou habilidade'));
+let poder2 = parseFloat(prompt('Digite o nome de mais um poder ou habilidade'));
+let poder3 = parseFloat(prompt('Digite o nome de outro um poder ou habilidade'));
+let poder4 = parseFloat(prompt('Digite o nome do ultimo poder ou habilidade'));
+let personagens = [protagonista, rival, pet, figurante];
+let poderes = [poder1, poder2, poder3, poder4];
 
 console.log(`Olá, ${nome}`);
 operaçãoSecreta(numero1, numero2);
@@ -21,5 +31,6 @@ function operaçãoSecreta(a, b) {
 function sorteio(lista){
     let quantidadeDeElementos = lista.lenght;
     let resultadoDoSorteio = parseInt(Math.random() * quantidadeDeElementos + 1);
+    console.log(`${resultadoDoSorteio}`);
     return resultadoDoSorteio;
 }
